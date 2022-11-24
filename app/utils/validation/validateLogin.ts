e0a02213118f367f60913046fd396ce0e09f27c2
Mayamee <ponyashcat228@gmail.com>
2022-11-24 12:26:48 +0300

    validate login jsdoc done

--- app/utils/validation/validateLogin.ts
+++ app/utils/validation/validateLogin.ts
@@ -2,6 +2,19 @@ import validator from 'validator'
 import ApiError from '../error/api-error'
 import customValidator from './customValidator'
 
+/**
+ * @function validateLogin
+ * @description validate login
+ * @param {string} login - login to validate
+ * @throws {ApiError} 400 - Login is empty
+ * @throws {ApiError} 400 - Login must be between 3 and 16 characters
+ * @throws {ApiError} 400 - Login must start with a letter
+ * @throws {ApiError} 400 - Login must contain only Latin letters and numbers
+ * @returns {void}
+ * @example
+ * validateLogin('test')
+ * validateLogin('test123')
+ */
 const validateLogin = (login: string): void => {
   if (validator.isEmpty(login)) {
     throw ApiError.badRequest('Login is empty')
