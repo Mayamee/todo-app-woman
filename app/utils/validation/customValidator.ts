123abc475948ea36966c9157dc2e49c176c12cc2
Mayamee <ponyashcat228@gmail.com>
2022-11-24 12:23:02 +0300

    custom validator jsdoc done

--- app/utils/validation/customValidator.ts
+++ app/utils/validation/customValidator.ts
@@ -1,3 +1,27 @@
+/**
+ * @class CustomValidator
+ * @classdesc class for validate some values
+ * @method isContainOnlyLatinLettersAndNumbers
+ * @description validate if the value contains only latin letters and numbers
+ * @param {string} value - value to validate
+ * @returns {boolean} true if the value contains only latin letters and numbers
+ * @example
+ * CustomValidator.isContainOnlyLatinLettersAndNumbers('test')
+ * CustomValidator.isContainOnlyLatinLettersAndNumbers('test123')
+ * @method isStartWithNumber
+ * @description validate if the value starts with number
+ * @param {string} value - value to validate
+ * @returns {boolean} true if the value starts with number
+ * @example
+ * CustomValidator.isStartWithNumber('1test')
+ * @method isContainOnlyDigits
+ * @description validate if the value contains only digits
+ * @param {string} value - value to validate
+ * @returns {boolean} true if the value contains only digits
+ * @example
+ * CustomValidator.isContainOnlyDigits('123')
+ */
+
 class CustomValidator {
   isContainOnlyLatinLettersAndNumbers(value: string) {
     return /^[a-zA-Z0-9]+$/.test(value)
