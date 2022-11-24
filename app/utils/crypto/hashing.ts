6a01598dbffa056e293ada4e6228e5384c4462eb
Mayamee <ponyashcat228@gmail.com>
2022-11-24 12:34:39 +0300

    hashing jsdoc done

--- app/utils/crypto/hashing.ts
+++ app/utils/crypto/hashing.ts
@@ -1,11 +1,27 @@
 import bcrypt from 'bcrypt'
-
-export const hashPassword = async (password: string) => {
+/**
+ * @function hashPassword
+ * @param {string} password - password to hash
+ * @returns {Promise<string>} hashed password
+ * @example
+ * hashPassword('test123')
+ */
+export const hashPassword = async (password: string): Promise<string> => {
   const salt = await bcrypt.genSalt(10)
   const hash = await bcrypt.hash(password, salt)
   return hash
 }
-export const comparePassword = async (password: string, hash: string) => {
+/**
+ * @function comparePassword
+ * @param {string} password - password to compare
+ * @param {string} hash - hash to compare
+ * @returns {Promise<boolean>} true if password hash and provided hash are equal
+ * @example
+ * comparePassword("123", "b8dhffn5bf").then((result) => {
+ *  console.log(result)
+ * })
+ */
+export const comparePassword = async (password: string, hash: string): Promise<boolean> => {
   const result = await bcrypt.compare(password, hash)
   return result
 }
