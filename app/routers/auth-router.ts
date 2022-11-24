16c11c37493a8039e631e4c7ea1f63a1ee3fa594
Mayamee <ponyashcat228@gmail.com>
2022-11-24 13:48:02 +0300

    auth router jsdoc done

--- app/routers/auth-router.ts
+++ app/routers/auth-router.ts
@@ -4,8 +4,34 @@ import {
   validateLoginRegisterMiddleware,
   validateRefreshTokenMiddleware,
 } from '../middleware/validate-middleware'
+/**
+ * @decription Auth routes
+ * @path /api/v1/auth
+ * @method POST
+ * @access Public
+ * @param {string} path - /register
+ * @param {function} middleware - validateLoginRegisterMiddleware
+ * @param {function} middleware - authController.register
+ *
+ * @method POST
+ * @access Public
+ * @param {string} path - /login
+ * @param {function} middleware - validateLoginRegisterMiddleware - validate login and password
+ * @param {function} middleware - authController.login - login user
+ *
+ * @method GET
+ * @access Public
+ * @param {string} path - /logout
+ * @param {function} middleware - validateRefreshTokenMiddleware - validate refresh token
+ * @param {function} middleware - authController.logout - logout user
+ *
+ * @method GET
+ * @access Public
+ * @param {string} path - /refresh
+ * @param {function} middleware - validateRefreshTokenMiddleware - validate refresh token
+ * @param {function} middleware - authController.refresh - refresh tokens
+ */
 const router = Router()
-
 router
   .post('/register', validateLoginRegisterMiddleware, authController.register)
   .post('/login', validateLoginRegisterMiddleware, authController.login)
