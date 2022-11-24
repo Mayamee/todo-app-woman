0c2fba95205cf491f0e09e69620afbd904d5abcb
Mayamee <ponyashcat228@gmail.com>
2022-11-24 14:31:57 +0300

    validate middleware jsdoc done

--- app/middleware/validate-middleware.ts
+++ app/middleware/validate-middleware.ts
@@ -9,11 +9,20 @@ import customValidator from '../utils/validation/customValidator'
 import IIdParamsRequest from '../types/controllers/IIdParamsRequest'
 import { isValidObjectId } from 'mongoose'
 
+/**
+ * @description Middleware for validating login and password from request body
+ * @function validateLoginRegisterMiddleware
+ * @param {IRegisterLoginRequest} req - request with login and password in body
+ * @param {Response} res - response
+ * @param {NextFunction} next - next function
+ * @returns {void}
+ *
+ */
 export function validateLoginRegisterMiddleware(
   req: IRegisterLoginRequest,
   _res: Response,
   next: NextFunction
-) {
+): void {
   try {
     const { login, password } = req.body
     validateLogin(login)
@@ -24,7 +33,20 @@ export function validateLoginRegisterMiddleware(
   }
 }
 
-export function validateRefreshTokenMiddleware(req: Request, _res: Response, next: NextFunction) {
+/**
+ * @description Middleware for validating Refresh Token from cookies
+ * @function validateRefreshTokenMiddleware
+ * @param {Request} req - request with refresh token in cookies
+ * @param {Response} res - response
+ * @param {NextFunction} next - next function
+ * @throws {ApiError} 401 - if refresh token is not provided
+ * @returns {void}
+ */
+export function validateRefreshTokenMiddleware(
+  req: Request,
+  _res: Response,
+  next: NextFunction
+): void {
   try {
     const { refreshToken } = req.cookies as { refreshToken: string | undefined }
     if (!refreshToken) {
@@ -36,11 +58,20 @@ export function validateRefreshTokenMiddleware(req: Request, _res: Response, nex
   }
 }
 
+/**
+ * @description Middleware for validating todo payload from request body
+ * @function validateTodoPalyoadMiddleware
+ * @param {ITodoPayloadRequest} req - request with todo payload in body
+ * @param {Response} res - response
+ * @param {NextFunction} next - next function
+ * @throws {ApiError} 400 - if todo payload is not valid
+ * @returns {void}
+ */
 export function validateTodoPalyoadMiddleware(
   req: ITodoPayloadRequest,
   _res: Response,
   next: NextFunction
-) {
+): void {
   try {
     const { title, description, todoBody } = req.body
     if (!title || !description || !todoBody) {
@@ -52,13 +83,22 @@ export function validateTodoPalyoadMiddleware(
   }
 }
 
+/**
+ * @description Middleware for validating query pagination params from request query
+ * If query params are not valid, default values are used
+ * @function validateGetAllTodosMiddleware
+ * @param {IGetAllTodoRequest} req - request with query pagination params
+ * @param {Response} res - response
+ * @param {NextFunction} next - next function
+ * @returns {void}
+ */
+
 export function validateGetAllTodosMiddleware(
   req: IGetAllTodoRequest,
   _res: Response,
   next: NextFunction
-) {
+): void {
   try {
-    //TODO refactor this
     const { limit, page } = req.query
     if (page && !customValidator.isContainOnlyDigits(page)) {
       req.query.page = undefined
@@ -78,11 +118,20 @@ export function validateGetAllTodosMiddleware(
   }
 }
 
+/**
+ * @description Middleware for validating todo id from request params
+ * @function validateTodoIdMiddleware
+ * @param {IIdParamsRequest} req - request with todo id in params
+ * @param {Response} res - response
+ * @param {NextFunction} next - next function
+ * @throws {ApiError} 400 - if todo id is not valid
+ * @returns {void}
+ */
 export function validateParamsIdMiddleware(
   req: IIdParamsRequest,
   _res: Response,
   next: NextFunction
-) {
+): void {
   try {
     const { id } = req.params
     if (!id || !isValidObjectId(id)) {
