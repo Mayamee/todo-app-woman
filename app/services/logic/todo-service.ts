912364504b96b315528d62603eadfb20bf7e76c3
Mayamee <ponyashcat228@gmail.com>
2022-11-24 13:39:38 +0300

    todo service jsdoc done

--- app/services/logic/todo-service.ts
+++ app/services/logic/todo-service.ts
@@ -2,6 +2,68 @@ import TodoModel from '../../models/Todo/TodoModel'
 import IGetTodoSettings from '../../types/services/IGetTodoSettings'
 import ITodoPayload from '../../types/services/ITodoPayload'
 
+/**
+ * @description Service for working with todo
+ * @class TodoService
+ * @method createTodo
+ * @description create todo
+ * @param {ITodoPayload} payload - payload for todo
+ * @returns {Promise<ITodoPayload>} created todo
+ * @example
+ * TodoService.createTodo({
+ * title: 'test',
+ * description: 'test',
+ * todoBody: 'test',
+ * ownerID: 'someObjectId',
+ * }).then((todo) => {
+ * console.log(todo)
+ * })
+ * getAllTodos
+ * @description get all todos
+ * @param ownerId - owner id
+ * @param {IGetTodoSettings} settings - settings for getting todos
+ * @returns {Promise<ITodoPayload[] | []>} todos
+ * @example
+ * TodoService.getAllTodos('someObjectId', {
+ * limit: 10,
+ * page: 1,
+ * }).then((todos) => {
+ * console.log(todos)
+ * })
+ * @method getTodoById
+ * @description get todo by id
+ * @param {string} ownerId - owner id
+ * @param {string} id - todo id
+ * @returns {Promise<ITodoPayload | null>} todo
+ * @example
+ * TodoService.getTodoById('someObjectId', 'someObjectId').then((todo) => {
+ * console.log(todo)
+ * })
+ * @method updateTodo
+ * @description update todo
+ * @param {string} ownerId - owner id
+ * @param {string} id - todo id
+ * @param {ITodoPayload} payload - payload for todo
+ * @returns {Promise<UpdateResult>} updated todo
+ * @example
+ * TodoService.updateTodo('someObjectId', 'someObjectId', {
+ * title: 'test',
+ * description: 'test',
+ * todoBody: 'test',
+ * ownerID: 'someObjectId',
+ * }).then((result) => {
+ * console.log(result)
+ * })
+ * @method deleteTodo
+ * @description delete todo
+ * @param {string} ownerId - owner id
+ * @param {string} id - todo id
+ * @returns {Promise<DeleteResult>} deleted todo
+ * @example
+ * TodoService.deleteTodo('someObjectId', 'someObjectId').then((result) => {
+ * console.log(result)
+ * })
+ */
 class TodoService {
   async createTodo(payload: ITodoPayload) {
     const { title, description, todoBody, ownerId } = payload
