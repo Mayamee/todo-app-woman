ad96e082c8bf406a39072342a00b4d632ad0055c
Mayamee <ponyashcat228@gmail.com>
2022-11-24 13:51:40 +0300

    todo router jsdoc done

--- app/routers/todo-router.ts
+++ app/routers/todo-router.ts
@@ -5,8 +5,37 @@ import {
   validateParamsIdMiddleware,
   validateTodoPalyoadMiddleware,
 } from '../middleware/validate-middleware'
+/**
+ * @decription Todo routes
+ * @path /api/v1/todo
+ * @method POST
+ * @access Private
+ * @param {string} path - /create
+ * @param {function} middleware - validateTodoPalyoadMiddleware - validate todo payload
+ * @param {function} middleware - todoController.createTodo - create todo
+ * @method GET
+ * @access Private
+ * @param {string} path - /all
+ * @param {function} middleware - validateGetAllTodosMiddleware - validate query params
+ * @param {function} middleware - todoController.getAllTodos - get all todos
+ * @method GET
+ * @access Private
+ * @param {string} path - /:id
+ * @param {function} middleware - validateParamsIdMiddleware - validate provided todo id
+ * @param {function} middleware - todoController.getTodoById - get todo by id
+ * @method PUT
+ * @access Private
+ * @param {string} path - /:id
+ * @param {function} middleware - validateParamsIdMiddleware - validate provided todo id
+ * @param {function} middleware - validateTodoPalyoadMiddleware - validate todo payload
+ * @param {function} middleware - todoController.updateTodo - update todo
+ * @method DELETE
+ * @access Private
+ * @param {string} path - /:id
+ * @param {function} middleware - validateParamsIdMiddleware - validate provided todo id
+ * @param {function} middleware - todoController.deleteTodo - delete todo
+ */
 const router = Router()
-
 router
   .post('/create', validateTodoPalyoadMiddleware, todoController.createTodo)
   .get('/all', validateGetAllTodosMiddleware, todoController.getAllTodos)
