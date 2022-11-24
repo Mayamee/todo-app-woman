9ea5d7c23a7f31d53ac06db3f09bf978f5da23bd
Mayamee <ponyashcat228@gmail.com>
2022-11-24 12:28:23 +0300

    validate password jsdoc done

--- app/utils/validation/validatePassword.ts
+++ app/utils/validation/validatePassword.ts
@@ -1,6 +1,17 @@
 import validator from 'validator'
 import ApiError from '../error/api-error'
 
+/**
+ * @function validatePassword
+ * @description validate password
+ * @param {string} password - password to validate
+ * @throws {ApiError} 400 - Password is empty
+ * @throws {ApiError} 400 - Password must contain at least 8 characters, 1 lowercase, 1 uppercase, 1 number and 1 symbol
+ * @returns {void}
+ * @example
+ * validatePassword('test123')
+ * validatePassword('test123!')
+ */
 const validatePassword = (password: string): void => {
   if (validator.isEmpty(password)) {
     throw ApiError.badRequest('Password is empty')
