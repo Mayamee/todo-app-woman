c8c5b37be351c67ccbf5e50a7eb2a3af99f29274
Mayamee <ponyashcat228@gmail.com>
2022-11-27 00:15:23 +0300

    fix root element ts

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,5 +2,6 @@ import { createRoot } from 'react-dom/client'
 import App from './App'
 import './main.scss'
 
-const container = document.getElementById('root') as HTMLElement
+const container = document.getElementById('root')
+if (!container) throw new Error('No root element found')
 createRoot(container).render(<App />)
