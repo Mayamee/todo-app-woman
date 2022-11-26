5e975e357ed00920e5a9a63e4e3e7a4bbb213867
Mayamee <ponyashcat228@gmail.com>
2022-11-26 16:33:10 +0300

    app build done

--- backend/scripts/copyPublicFolder.js
+++ backend/scripts/copyPublicFolder.js
@@ -6,7 +6,7 @@ copyfiles([`app/public/**/*`, 'build/app'], { up: 1, error: true }, (err) => {
     console.info('Problem copying public folder content:', err.message)
     mkdirp('build/app/public')
       .then(() => {
-        console.info('Public folder created in build/app.')
+        console.info('Backend public folder created in build/app.')
       })
       .catch((err) => {
         console.info('Problem creating public folder in build/app', err.message, '\n')
