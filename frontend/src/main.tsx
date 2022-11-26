f588d3ac775b0bbf57622f3c51560fef9ca28994
Mayamee <ponyashcat228@gmail.com>
2022-11-27 02:53:53 +0300

    install redux

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,7 +1,17 @@
+import { BrowserRouter as ReactRouter } from 'react-router-dom'
 import { createRoot } from 'react-dom/client'
 import App from './App'
 import './main.scss'
+import Layout from './containers/Layout/Layout'
+
+const app = (
+  <ReactRouter>
+    <Layout>
+      <App />
+    </Layout>
+  </ReactRouter>
+)
 
 const container = document.getElementById('root')
 if (!container) throw new Error('No root element found')
-createRoot(container).render(<App />)
+createRoot(container).render(app)
