c59dd4d9ce6c93a4d8eacd2c5a4c473a0666846b
Mayamee <ponyashcat228@gmail.com>
2022-11-27 20:24:57 +0300

    add logo

--- frontend/src/components/ToolBar/ToolBar.tsx
+++ frontend/src/components/ToolBar/ToolBar.tsx
@@ -1,5 +1,7 @@
 import { FC } from 'react'
 import styles from './ToolBar.module.scss'
+import { ReactComponent as LogoIcon } from '../../assets/images/logo.svg'
+import SvgContainer from '../../containers/SvgContainer/SvgContainer'
 
 interface IToolBarProps {}
 
@@ -7,7 +9,9 @@ const ToolBar: FC<IToolBarProps> = () => {
   return (
     <div className={styles['toolbar-body']}>
       <div className={styles['toolbar-body-menuButton']}>Menu</div>
-      <div className={styles['toolbar-body-logo']}>Logo</div>
+      <div className={styles['toolbar-body-logo']}>
+        <SvgContainer size={50} icon={<LogoIcon />} />
+      </div>
       <div className={styles['toolbar-body-search']}>Search</div>
       <div className={styles['toolbar-body-avatar']}>Avatar</div>
     </div>
