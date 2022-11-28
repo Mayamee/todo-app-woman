46e73f8215dadac5de3a3822dda8095a894ebb21
Mayamee <ponyashcat228@gmail.com>
2022-11-28 23:32:48 +0300

    addaptive toolbar

---
+++ frontend/src/constants/MediaQueries.ts
@@ -0,0 +1,7 @@
+export default {
+  desktop: '(max-width: 1200px)',
+  laptop: '(max-width: 992px)',
+  tablet: '(max-width: 768px)',
+  bigPhone: '(max-width: 576px)',
+  phone: '(max-width: 480px)',
+}
