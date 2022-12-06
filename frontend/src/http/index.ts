744ba8033d20ecc209809db71b3aa14376c6b6c5
Mayamee <ponyashcat228@gmail.com>
2022-12-06 03:10:45 +0300

    remove comments

--- frontend/src/http/index.ts
+++ frontend/src/http/index.ts
@@ -18,20 +18,15 @@ API.interceptors.response.use(
   async (error) => {
     const originalRequest = error.config
     if (error.response.status === 401 && originalRequest && !originalRequest._retry) {
-      // чтобы избежать зацикливания при повторной попытке авторизации добавили флаг обработки
       originalRequest._retry = true
       try {
-        // если не авторизован, то пробуем получить новый accessToken
         const response = await axios.get<IAuthModel>(`${API_URL}/auth/refresh`, {
           withCredentials: true,
         })
-        // записываем новый accessToken в localStorage
         localStorage.setItem('token', response.data.accessToken)
         console.log('Original request: ', originalRequest)
-        // повторяем запрос пользователя
         return API.request(originalRequest)
       } catch (error) {
-        // если не удалось получить новый accessToken, то перенаправляем на страницу авторизации
         console.log('Not authorized')
       }
     }
